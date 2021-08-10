19344b304e33ba3d95c09ca8a3d4226058354ce1
mray1995 <sleepingbag5927@gmail.com>
2021-08-09 21:06:11 -0500

    Header and navbar added.

---
+++ src/components/Header.js
@@ -0,0 +1,12 @@
+import Navigation from './Navigation';
+
+function Header() {
+    return (
+        <header className="container-fluid">
+        <h1>Matt Ray</h1>
+        
+        <Navigation />
+        </header>
+    )
+}
+export default Header;
---
+++ src/components/Navigation.js
@@ -0,0 +1,13 @@
+
+
+function Navigation() {
+    return (
+        <nav className="nav justify-content-between">
+  <a className="nav-link "  href="#">About me</a>
+  <a className="nav-link" href="#">Portfolio</a>
+  <a className="nav-link" href="#">Resume</a>
+  <a className="nav-link" href="#">Contact me</a>
+</nav>
+    )
+}
+export default Navigation;
